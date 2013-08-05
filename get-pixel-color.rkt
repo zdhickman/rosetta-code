e22e35fea1f5026591f31c844b5991f67df72dab
zdhickman <zdhickman@gmail.com>
2013-08-04 20:34:40 -0400

    Revised unnecessary cond

--- get-pixel-color.rkt
+++ get-pixel-color.rkt
@@ -12,8 +12,9 @@
 
 (define (do-color w x y k)
   (define g (get-pixel-color x y PICTURE))
-  (cond [(mouse=? "button-down" k) (printf (string-append (color->string g) "\n")) g]
-        [else g]))
+  (when (mouse=? "button-down" k) 
+    (printf (string-append (color->string g) "\n")))
+  g)
 
 (big-bang (color 255 255 255 0)
           (to-draw (lambda (w) PICTURE))
